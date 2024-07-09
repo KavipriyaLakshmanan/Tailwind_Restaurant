f924c45c0d7809887f6fb5185ad6d8344d065b81
KavipriyaLakshmanan <Kavipriya.L@3analytics.com>
2024-07-09 13:53:45 +0530

    unwanted code removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
 import Layout from "./restaurant/Layout";
 import Restaurant from "./restaurant/Restaurant";
-// import Navbar from "./components/Navbar";
-// import Practice1 from "./components/Practice1";
-// import Slidebar from "./components/Slidebar";
-// import TwRadio from "./components/TwRadio";
 
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Practice1 />
-      <Slidebar />
-      <TwRadio /> */}
       <Layout />
       <Restaurant />
     </>
